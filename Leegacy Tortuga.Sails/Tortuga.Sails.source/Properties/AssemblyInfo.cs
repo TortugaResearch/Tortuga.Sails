bfef1788ebca4ff9e02fbf73392ab1cb8647c3bf
Jonathan Allen <grauenwolf@gmail.com>
2019-10-31 19:42:34 -0700

    Remove legacy version

--- Leegacy Tortuga.Sails/Tortuga.Sails.source/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-
-[assembly: AssemblyConfiguration("Developer build. Not for production use.")]
-[assembly: ComVisible(false)]
-[assembly: Guid("ad24fb72-7191-4bec-be2e-82cab1a333ce")]
-
--- Leegacy Tortuga.Sails/Tortuga.Sails.source/Properties/SharedAssemblyInfo.cs.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Resources;
-
-[assembly: AssemblyTitle("Tortuga.Sails")]
-[assembly: AssemblyDescription("A library for XAML and MVVM style applications")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.2.*")]
-[assembly: NeutralResourcesLanguage("")]
